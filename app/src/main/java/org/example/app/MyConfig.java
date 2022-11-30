811b28860c55c977e8e923ea04b753d5a006d019
Ablazzing <y2286122@yandex.ru>
2022-11-30 11:15:00 +0300

    Многомодульный проект

--- app/src/main/java/org/example/app/MyConfig.java
+++ app/src/main/java/org/example/app/MyConfig.java
@@ -4,8 +4,10 @@ import org.example.service.EmployeeService;
 import org.example.service.WorkerService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Import({EmployeeService.class, WorkerService.class})
+@PropertySource("example.properties")
 public class MyConfig {
 }
--- app/src/main/java/org/example/app/TestController.java
+++ app/src/main/java/org/example/app/TestController.java
@@ -2,6 +2,7 @@ package org.example.app;
 
 import org.example.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,14 @@ public class TestController {
     @Autowired
     ApplicationContext applicationContext;
 
+    //Конструкция #{} - позволяет исполнять java код. ${} - выбирает свойство из пропертей (селектор)
+    //подходит когда нам нужно получить экземпляр определенного класса
+    @Value("#{new Integer('${petr.age}')}")
+    Integer petrAge;
+    @Value("#{new Integer('${yuri.age}')}")
+    Integer age;
+
+
     @GetMapping("/test")
     public String test() {
         Map<String, TestController> beansOfType = applicationContext.getBeansOfType(TestController.class);
@@ -24,5 +33,14 @@ public class TestController {
     @GetMapping("/count")
     public Integer countEmployees() {
         return employeeService.countEmployees();
+    }
+
+    @GetMapping("/age")
+    public Integer getAge() {
+        return age;
+    }
+    @GetMapping("/petr_age")
+    public Integer getPetrAge() {
+        return petrAge;
     }
 }
